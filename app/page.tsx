afb71aac91f6bc9888171d95c9ee0da09da39879
Tom Oliveira <tom.o@outlook.com.br>
2025-08-03 13:28:13 -0300

    Corrige erro de build da Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-import { LlmInterface } from "@/components/llm-interface"
+import dynamic from "next/dynamic"
+
+// Usamos dynamic import para carregar o componente apenas no lado do cliente
+const LlmInterface = dynamic(() => import("@/components/llm-interface").then((mod) => mod.LlmInterface), {
+  ssr: false, // ssr: false desativa a renderização no servidor
+  loading: () => (
+    <p className="flex h-screen items-center justify-center bg-black text-white">Carregando Interface...</p>
+  ),
+})
 
 export default function Home() {
   return (
